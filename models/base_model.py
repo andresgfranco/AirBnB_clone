56d37a73b61270a6cfb458385956d07157cf002d
andresgfranco <andresgfranco0@gmail.com>
2021-02-13 18:03:35 +0000

    datetime for update_at and create_at corrected, converting them to string when to_dict

--- models/base_model.py
+++ models/base_model.py
@@ -15,20 +15,21 @@ class BaseModel:
                 self.id = kwargs["id"]
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now().isoformat()
-            self.updated_at = datetime.now().isoformat()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """ String representation of object """
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
                                      self.__dict__)
-
     def save(self):
         """ Saves """
-        self.updated_at = datetime.now().isoformat()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """ Returns a dictionary containing key/values of an instance """
+        self.updated_at = str(self.updated_at.isoformat())
+        self.created_at = str(self.created_at.isoformat())
         new_dict = self.__dict__.copy()
         new_dict['__class__'] = self.__class__.__name__
         return new_dict
