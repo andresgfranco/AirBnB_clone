75f96da705a4accf8ef22e1753719e7667144a21
andresgfranco <andresgfranco0@gmail.com>
2021-02-16 17:18:52 +0000

    Adding pep8 correction test

--- tests/test_engine/test_file_storage.py
+++ tests/test_engine/test_file_storage.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 """Module for testing FileStorage class"""
 import unittest
+import pep8
 
 
 class TestFileStorage(unittest.Testcase):
     """Class for testing FileStorage class"""
-
-    def
+    def test_pep8_conformance_base(self):
+        """pep8 tests for FileStorage"""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/file_storage.py'])
+        self.assertEqual(result.total_errors, 0,
+                "Found code style errors (and warnings).")
