9b9d0343bf430a2621755a51df798e2a2bbd0f22
andresgfranco <andresgfranco0@gmail.com>
2021-02-16 17:14:29 +0000

    Adding test files for file storage class

---
+++ tests/test_engine/test_file_storage.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for testing FileStorage class"""
+import unittest
+
+
+class TestFileStorage(unittest.Testcase):
+    """Class for testing FileStorage class"""
+
+    def
