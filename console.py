1e0d18b089391dab813208f2ff5ac3d719966f0c
andresgfranco <andresgfranco0@gmail.com>
2021-02-17 00:22:49 +0000

    Correction pep8 style

--- console.py
+++ console.py
@@ -15,7 +15,7 @@ from models.review import Review
 class HBNBCommand(cmd.Cmd):
     """Class HBNBCommand
     for command interpreter"""
-    __classes = { 
+    __classes = {
             "BaseModel": BaseModel,
             "User": User,
             "State": State,
@@ -128,7 +128,6 @@ class HBNBCommand(cmd.Cmd):
                 existing_instances[instance_id].save()
             else:
                 print("** no instance found **")
-
 
     def __class_id_checker(self, line_args, len_args):
         """ Checks if class name and id exist """
